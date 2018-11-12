275ab7d43537ec9802f0babd87e2e732713da936
seankrummel <sean.krummel@gmail.com>
2018-11-12 13:52:28 -0600

    created GET notes list and details endpoints

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const express = require('express');
 const data = require('./db/notes');
 const app = express();
 
-console.log('Hello Noteful!');
+// console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
 
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => res.json(data));
+app.get('/api/notes/:id', (req, res) => {
+  res.json(data.find(note => note.id === Number(req.params.id)));
+});
 
 app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
