24b129ca8d50108b70a14e84b77219c85ee60f01
seankrummel <sean.krummel@gmail.com>
2018-11-14 13:42:07 -0600

    create new post endpoint

--- router/notes.router.js
+++ router/notes.router.js
@@ -39,4 +39,21 @@ router.put('/:id', (req, res, next) => {
   });
 });
 
+router.post('/', (req, res, next) => {
+  const {title, content} = req.body;
+  
+  const newItem = {title, content};
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status(404);
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) return next(err);
+    if (item) res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    else next();
+  });
+});
+
 module.exports = router;
